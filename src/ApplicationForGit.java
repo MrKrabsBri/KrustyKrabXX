80b8e297829386cc2c735a244b5d94cd0c9e7472
webuser <JuliusBri@Outlook.com>
2022-11-12 23:44:50 +0200

    FirSt cOmMiT1

---
+++ src/ApplicationForGit.java
@@ -0,0 +1,11 @@
+
+public class ApplicationForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello worlddddDDDDDddddd");
+
+	}
+
+}
